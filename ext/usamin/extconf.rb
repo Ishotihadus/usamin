0c601a2cfa47da0736289e400e16275694429d5e
Ishotihadus <hanachan.pao@gmail.com>
2018-03-02 10:37:12 +0900

    improve extconf

--- ext/usamin/extconf.rb
+++ ext/usamin/extconf.rb
@@ -1,6 +1,11 @@
 require 'mkmf'
 
+RbConfig::MAKEFILE_CONFIG['CXX'] = ENV['CXX'] if ENV['CXX']
 have_library('stdc++')
 dir_config('rapidjson')
-$CPPFLAGS << ' -O3 -Wall -Wextra -Wvla -std=c++14'
+append_cppflags('-O3')
+append_cppflags('-Wall')
+append_cppflags('-Wextra')
+append_cppflags('-Wvla')
+$CXXFLAGS << ' -std=c++11'
 create_makefile('usamin/usamin')
