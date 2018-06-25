2e2d6cbcf053444f82d22925c0329060b3c6df84
Ishotihadus <hanachan.pao@gmail.com>
2018-06-26 01:01:35 +0900

    support simd optimization

--- ext/usamin/extconf.rb
+++ ext/usamin/extconf.rb
@@ -5,7 +5,14 @@ have_library('stdc++')
 dir_config('rapidjson')
 append_cppflags('-O3')
 append_cppflags('-Wall')
-append_cppflags('-Wextra')
-append_cppflags('-Wvla')
-$CXXFLAGS << ' -std=c++11'
+
+if checking_for("whether -march=native is accepted as CPPFLAGS"){try_cppflags('-march=native')}
+    if checking_for("whether -msse4.2 is accepted as CPPFLAGS"){try_cppflags('-msse4.2')}
+        $CPPFLAGS << ' -msse4.2 -march=native'
+    elsif checking_for("whether -msse2 is accepted as CPPFLAGS"){try_cppflags('-msse2')}
+        $CPPFLAGS << ' -msse2 -march=native'
+    end
+end
+
+$CPPFLAGS << ' -std=c++11'
 create_makefile('usamin/usamin')
